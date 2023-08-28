6205291b32af9fd7553d415631c29db017b25ec8
Sara Petrovic <petrovic.ra21.2019@uns.ac.rs>
2023-08-29 01:07:11 +0200

    Implemented export club members data to excel

--- frontend-application/src/app/components/medals/medals.component.ts
+++ frontend-application/src/app/components/medals/medals.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {DisciplineCompetition} from "../../models/discipline-competition.model";
+import {Component, OnInit} from '@angular/core';
+import * as XLSX from 'xlsx';
 import {Medal} from "../../models/medal.model";
 import {MatTableDataSource} from "@angular/material/table";
 import {User} from "../../models/user.model";
@@ -16,6 +16,7 @@ export class MedalsComponent implements OnInit {
   public dataSourceMedals= new MatTableDataSource<Medal>();
   public displayedColumnsMedals = ['competitionName', 'date', 'medal', 'student', 'discipline', 'gender category', 'group category', 'weight category'];
   public loggedInUser: User;
+  fileName= 'ExcelSheet.xlsx';
   constructor(private competitionService: CompetitionService, private userService: UserService) { }
 
   ngOnInit(): void {
@@ -25,5 +26,27 @@ export class MedalsComponent implements OnInit {
       this.dataSourceMedals.data = this.medals;
     })
   }
+  exportExcel(): void
+  {
+    /* pass here the table id */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+    /* generate workbook and add the worksheet */
+    ws['!cols'] = [
+      {wch: 25},
+      {wch: 10},
+      {wch: 10},
+      {wch: 20},
+      {wch: 15},
+      {wch: 15},
+      {wch: 15},
+      {wch: 15}
+    ];
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+
+  }
 }
